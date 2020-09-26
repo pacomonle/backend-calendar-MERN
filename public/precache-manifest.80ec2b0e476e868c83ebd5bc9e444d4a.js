65a5c713e7e66ea3f1671de7abe9cc400f61cff9
pacomonle <franciscomonleonleal@gmail.com>
2020-09-27 01:31:02 +0200

    depliegue a produccion

---
+++ public/precache-manifest.80ec2b0e476e868c83ebd5bc9e444d4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba6423002dbed1143ce89ff8ea3d823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e9ecb8b11660045b390",
+    "url": "/static/css/2.dabbbe1b.chunk.css"
+  },
+  {
+    "revision": "c11f711f5e3567756909",
+    "url": "/static/css/main.00b6d57c.chunk.css"
+  },
+  {
+    "revision": "9e9ecb8b11660045b390",
+    "url": "/static/js/2.b42b0cb5.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.b42b0cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c11f711f5e3567756909",
+    "url": "/static/js/main.63707aa8.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
